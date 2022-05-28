6bcce55333072f0b943a583e8a04ad2b6b279cb3
szoboszlaianna <szoboszlai.anna0@gmail.com>
2022-05-28 16:20:26 +0200

    Current stage. todo: use params.

--- scrapper.js
+++ scrapper.js
@@ -1,36 +1,48 @@
 const puppeteer = require('puppeteer');
 const ObjectsToCsv = require('objects-to-csv');
 
+const params = {
+  cities: ['københavn', 'S%C3%B8borg', 'kongens-lyngby', 'herlev', 'ballerup', 'Rødovre', 'bagsvaerd', 'gentofte'],
+  max_rent: 13500,
+  min_size: 60,
+  months: ['oktober', 'november'],
+};
+
 (async () => {
   const browser = await puppeteer.launch();
-  console.log('Started');
+  console.log('Scrapper started');
   const page = await browser.newPage();
-  cities = ['S%C3%B8borg', 'kongens-lyngby', 'herlev', 'ballerup', 'Rødovre', 'bagsvaerd', 'gentofte'];
-  await page.goto('https://www.boligportal.dk/lejligheder/k%C3%B8benhavn/2-3-v%C3%A6relser/?min_rental_period=12&max_monthly_rent=13500&min_size_m2=60');
+  await page.goto('https://www.boligportal.dk/');
   const cookieAcceptBtn = await page.$('.coi-banner__accept');
   await cookieAcceptBtn.click();
-  const hrefs = await page.$$eval('.AdCardSrp__Link', as => as.map(a => a.href));
   data = [];
-  for (let href of hrefs) {
-    console.log(href);
-    await page.goto(href);
-    const priceElement = await page.waitForSelector('.css-1p985l2');
-    const price = await priceElement.evaluate(el => el.textContent);
-    const infoBox = await page.$$eval('.css-sg80e', as => as.map(a => a.textContent));
-    const period = infoBox[2];
-    const dateElement = await page.waitForSelector('.css-1xqwnoo span');
-    const date = await dateElement.evaluate(el => el.textContent);
-    await page.waitForSelector('.css-1sdzuqu');
-    const others = await page.$$eval('.css-1sdzuqu', as => as.map(a => a.textContent));
-    location = others[1];
-    const elementValue = await page.$$eval('.css-uzgi25', as => as.map(a => a.textContent));
-    line = { price: price, area: elementValue[1], rooms: elementValue[2], date: date, period: period, location: location, href: href };
-    if (line.period != '1-11 måneder') {
-      data.push(line);
+  for (let city of params.cities) {
+    console.log(city);
+    await page.goto(`https://www.boligportal.dk/lejligheder/${city}/2-3-v%C3%A6relser/?min_rental_period=12&max_monthly_rent=${params.max_rent}&min_size_m2=${params.min_size}`);
+    const hrefs = await page.$$eval('.AdCardSrp__Link', as => as.map(a => a.href));
+    console.log(`Fetched links ${hrefs.length}`);
+    for (let href of hrefs) {
+      await page.goto(href);
+      const priceElement = await page.waitForSelector('.css-1p985l2');
+      const price = await priceElement.evaluate(el => el.textContent);
+      const infoBox = await page.$$eval('.css-sg80e', as => as.map(a => a.textContent));
+      const period = infoBox[2];
+      const dateElement = await page.waitForSelector('.css-1xqwnoo span');
+      const date = await dateElement.evaluate(el => el.textContent);
+      await page.waitForSelector('.css-1sdzuqu');
+      const others = await page.$$eval('.css-1sdzuqu', as => as.map(a => a.textContent));
+      location = others[1];
+      const elementValue = await page.$$eval('.css-uzgi25', as => as.map(a => a.textContent));
+      line = { price: price, area: elementValue[1], rooms: elementValue[2], date: date, period: period, location: location, href: href };
+      if (params.months.some(month => date.includes(month))) {
+        console.log(line);
+        data.push(line);
+      }
     }
   }
+  console.log(data);
   const csv = new ObjectsToCsv(data);
-  await csv.toDisk('./test.csv');
+  await csv.toDisk('./apartments2.csv');
   console.log(await csv.toString());
   await browser.close();
 })();
