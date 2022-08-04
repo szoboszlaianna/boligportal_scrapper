6f83e337bff23ede59c1c0c9327f29a59a92135d
szoboszlaianna <szoboszlai.anna0@gmail.com>
2022-08-04 22:27:58 +0200

    Current.

---
+++ params.js
@@ -0,0 +1,6 @@
+const params = {
+  cities: ['københavn', 'S%C3%B8borg', 'kongens-lyngby', 'herlev', 'ballerup', 'Rødovre', 'bagsvaerd', 'gentofte'],
+  max_rent: 13500,
+  min_size: 60,
+  months: ['oktober', 'november'],
+};
--- scrapper.js
+++ scrapper.js
@@ -2,10 +2,10 @@ const puppeteer = require('puppeteer');
 const ObjectsToCsv = require('objects-to-csv');
 
 const params = {
-  cities: ['københavn', 'S%C3%B8borg', 'kongens-lyngby', 'herlev', 'ballerup', 'Rødovre', 'bagsvaerd', 'gentofte'],
-  max_rent: 13500,
+  cities: ['københavn', 'S%C3%B8borg', 'kongens-lyngby', 'bagsvaerd', 'gentofte', 'brønshøj'],
+  max_rent: 13700,
   min_size: 60,
-  months: ['oktober', 'november'],
+  months: ['october', 'november', 'december', 'januar'],
 };
 
 (async () => {
@@ -21,18 +21,20 @@ const params = {
     await page.goto(`https://www.boligportal.dk/lejligheder/${city}/2-3-v%C3%A6relser/?min_rental_period=12&max_monthly_rent=${params.max_rent}&min_size_m2=${params.min_size}`);
     const hrefs = await page.$$eval('.AdCardSrp__Link', as => as.map(a => a.href));
     console.log(`Fetched links ${hrefs.length}`);
+    const buttonText = await page.$$eval('.css-150kjhb css-z3633x', as => as.map(a => a.textContent));
+    console.log(buttonText);
     for (let href of hrefs) {
+      console.log(href);
       await page.goto(href);
-      const priceElement = await page.waitForSelector('.css-1p985l2');
+      const priceElement = await page.waitForSelector('.css-goiemm');
       const price = await priceElement.evaluate(el => el.textContent);
-      const infoBox = await page.$$eval('.css-sg80e', as => as.map(a => a.textContent));
+      const infoBox = await page.$$eval('.css-15kk0v4', as => as.map(a => a.textContent));
       const period = infoBox[2];
-      const dateElement = await page.waitForSelector('.css-1xqwnoo span');
+      const dateElement = await page.waitForSelector('.css-apmxeo span');
       const date = await dateElement.evaluate(el => el.textContent);
-      await page.waitForSelector('.css-1sdzuqu');
-      const others = await page.$$eval('.css-1sdzuqu', as => as.map(a => a.textContent));
+      const others = await page.$$eval('.css-1bbi9fj', as => as.map(a => a.textContent));
       location = others[1];
-      const elementValue = await page.$$eval('.css-uzgi25', as => as.map(a => a.textContent));
+      const elementValue = await page.$$eval('.css-1e8e3fr', as => as.map(a => a.textContent));
       line = { price: price, area: elementValue[1], rooms: elementValue[2], date: date, period: period, location: location, href: href };
       if (params.months.some(month => date.includes(month))) {
         console.log(line);
